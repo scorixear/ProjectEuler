cf41514669b241ec98354782800d53084f7c6ee4
Paul Keller <pkeller1@stud.hs-heilbronn.de>
2018-12-25 10:16:28 +0100

    added problem 16

--- src/problem16/PowerOfTwo.java
+++ src/problem16/PowerOfTwo.java
@@ -1,9 +1,23 @@
 package problem16;
 
+import java.math.BigInteger;
+
 /**
  * @author Paul Keller
  * @version 1.0
  */
 public class PowerOfTwo {
+  public static void main(String[] args) {
+    BigInteger num = BigInteger.ONE;
+    for(int i = 1;i<=1000;i++){
+      num=num.multiply(BigInteger.TWO);
+    }
+    String s = num.toString();
+    long count = 0;
+    for(char c:s.toCharArray()){
+      count = count + Long.valueOf(c+"");
+    }
+    System.out.println(count);
+  }
 
 }
---
+++ src/problem17/NumberLetter.java
@@ -0,0 +1,9 @@
+package problem17;
+
+/**
+ * @author Paul Keller
+ * @version 1.0
+ */
+public class NumberLetter {
+
+}
