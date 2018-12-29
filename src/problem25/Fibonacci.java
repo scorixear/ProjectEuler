74f867c27f199a59f50ab1edad6f860a12426666
Paul Keller <pkeller1@stud.hs-heilbronn.de>
2018-12-29 11:48:50 +0100

    added problem 25

--- src/problem25/Fibonacci.java
+++ src/problem25/Fibonacci.java
@@ -1,9 +1,30 @@
 package problem25;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+
 /**
  * @author Paul Keller
  * @version 1.0
  */
 public class Fibonacci {
+
+  private static ArrayList<BigInteger> fib;
+
+  public static void main(String[] args) {
+    fib = new ArrayList<>();
+    fib.add(BigInteger.ONE);
+    fib.add(BigInteger.ONE);
+    int i = 2;
+    while (true) {
+      BigInteger nextFib = fib.get(i-1).add(fib.get(i-2));
+      fib.add(nextFib);
+      if(nextFib.toString().length()>=1000){
+        break;
+      }
+      i++;
+    }
+    System.out.println(fib.get(fib.size()-1)+" "+i);
+  }
 
 }
