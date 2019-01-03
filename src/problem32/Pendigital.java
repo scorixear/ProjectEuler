10ae0cc572ddd47f5394b177c9c9b4548d560b47
Paul Keller <pkeller1@stud.hs-heilbronn.de>
2019-01-03 17:19:33 +0100

    added problem 32

---
+++ src/problem32/Pendigital.java
@@ -0,0 +1,32 @@
+package problem32;
+
+import java.util.ArrayList;
+
+/**
+ * @author Paul Keller
+ * @version 1.0
+ */
+public class Pendigital {
+  public static void main(String[] args) {
+    ArrayList<Long> products = new ArrayList<>();
+    long sum = 0;
+    for(long a = 1;a<10000;a++){
+      for(long b = 1;b<10000;b++){
+        long product = a*b;
+
+        if(isPendigital(a,b,product)&& !products.contains(product)){
+          products.add(product);
+          sum+=product;
+          System.out.println(a+" * "+b+" = "+product);
+        }
+      }
+    }
+    System.out.println(sum);
+  }
+
+  private static boolean isPendigital(long a, long b, long c) {
+    String s = a+""+b+""+c;
+    return s.matches("^(?:([1-9])(?!.*\\1)){9}$");
+  }
+
+}
